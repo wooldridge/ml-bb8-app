12e44b196f3079a046062a7887311415fc72c52e
wooldridge <mike@hellion.org>
2022-12-29 14:41:53 -0800

    Add setup and proxy features
    setup.js configures server and loads documents.
    proxy.js is an Express-based proxy to MarkLogic that the UI accesses.
    Document data is now loaded dynamically from the database (see PanelContext.jsx and api.js).
    README has setup instructions.

---
+++ server/proxy.js
@@ -0,0 +1,32 @@
+const config = require('../config');
+const express = require('express');
+const morgan = require("morgan");
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+// Create Express Server
+const app = express();
+
+// Configuration
+const PORT = config.server.port;
+const HOST = config.host;
+const API_SERVICE_URL = "http://" + config.host + ":" + config.rest["rest-api"].port;
+
+// Logging
+app.use(morgan('dev'));
+
+// Info GET endpoint
+app.get('/info', (req, res, next) => {
+    res.send('This service proxies to the MarkLogic REST API.');
+});
+
+// Proxy endpoints
+app.use('/v1/documents', createProxyMiddleware({
+    target: API_SERVICE_URL,
+    changeOrigin: true,
+    auth: config.user["user-name"] + ":" + config.user.password
+}));
+
+// Start Proxy
+app.listen(PORT, HOST, () => {
+    console.log(`Starting Proxy at ${HOST}:${PORT}`);
+});
