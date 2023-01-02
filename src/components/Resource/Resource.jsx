63f0351c98ff9c0fb319e324fe067fb75c98d98d
Griffin Wooldridge <Griffin.Wooldridge@marklogic.com>
2023-01-01 21:18:31 -0800

    Image files

---
+++ src/components/Resource/Resource.jsx
@@ -0,0 +1,148 @@
+import { ArrowRight, Cross, Minus } from 'akar-icons';
+import Javelin from '../../images/javelin.png';
+import BarsHigh from '../../images/bars-high.svg';
+
+import './Resource.css';
+
+function Resource() {
+  const handleTabClick = (event) => {
+    // Handle tabs
+    const allTabs = Array.from(document.getElementsByClassName('nav-link'));
+    allTabs.forEach((tab) => {
+      tab.classList.remove('active');
+    });
+    const clickedTab = document.getElementById(event.target.id);
+    clickedTab.classList.add('active');
+
+    // Handle tab content
+    const allContent = Array.from(
+      document.getElementsByClassName('tab-content')
+    );
+    allContent.forEach((section) => {
+      section.classList.remove('selected');
+    });
+    const contentToShow = document.getElementById(event.target.id + '-content');
+    contentToShow.classList.add('selected');
+  };
+  return (
+    <div className='card-convoy card col-4 me-2 mt-2'>
+      <div className='card-body'>
+        <div className='row details-top justify-content-between request-fields g-0'>
+          <nav aria-label='breadcrumb'>
+            <ol className='breadcrumb pt-1'>
+              <li className='breadcrumb-item'>Zhytomyr</li>
+              <li className='breadcrumb-item'>Stock informations</li>
+              <li className='breadcrumb-item active'>foo</li>
+            </ol>
+          </nav>
+          <div className='action-icons'>
+            <Minus strokeWidth={2} size={16} />
+            <Cross className='ms-3' strokeWidth={2} size={16} />
+          </div>
+        </div>
+
+        <div className='row justify-content-between pb-3 mt-3 g-0'>
+          <div className='col-xl-6 col-lg-12 p-1'>
+            <h5 className='card-title fs-4'>Javelin missles</h5>
+            <div className='form-label'>Anti-tank rockets</div>
+            <div className='description fs-7'>
+              The FGM-148 Javelin, or Advanced Anti-Tank Weapon System-Medium
+              (AAWS-M)
+            </div>
+          </div>
+          <img src={Javelin} className='img-javelin col-xl-6 p-1' />
+        </div>
+
+        <ul className='nav card-nav mb-3'>
+          <li className='nav-item'>
+            <a
+              id='tab-stock-level'
+              className='nav-link px-0 me-2 active'
+              aria-current='page'
+              href='#'
+              onClick={handleTabClick}
+            >
+              Stock Level
+            </a>
+          </li>
+          <li className='nav-item'>
+            <a
+              id='tab-units'
+              className='nav-link px-0 mx-2'
+              href='#'
+              onClick={handleTabClick}
+            >
+              Units
+            </a>
+          </li>
+          <li className='nav-item'>
+            <a
+              id='tab-media'
+              className='nav-link px-0 mx-2'
+              href='#'
+              onClick={handleTabClick}
+            >
+              Media
+            </a>
+          </li>
+          <li className='nav-item'>
+            <a
+              id='tab-staff'
+              className='nav-link px-0 mx-2'
+              href='#'
+              onClick={handleTabClick}
+            >
+              Staff
+            </a>
+          </li>
+          <li className='nav-item'>
+            <a
+              id='tab-infrastructure'
+              className='nav-link px-0 mx-2'
+              href='#'
+              onClick={handleTabClick}
+            >
+              Infrastructure
+            </a>
+          </li>
+          <li className='nav-item'>
+            <a
+              id='tab-vehicles'
+              className='nav-link px-0 mx-2'
+              href='#'
+              onClick={handleTabClick}
+            >
+              Vehicles
+            </a>
+          </li>
+        </ul>
+
+        <div
+          id='tab-stock-level-content'
+          className='tab-content stock-level selected'
+        >
+          <div className='row justify-content-between'>
+            <div className='form-label width-auto'>Warehouse</div>
+            <div className='form-label width-auto'>Number of Items</div>
+          </div>
+
+          <div className='row justify-content-between'>
+            <div className='row width-auto'>
+              <img src={BarsHigh} className='width-auto' />
+              <div className='width-auto'>MWH Kiev 1</div>
+            </div>
+
+            <div className='row width-auto'>
+              <div className='width-auto'>150</div>
+              <span className='material-symbols-outlined width-auto'>
+                arrow_forward
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Resource;
